8b486d294e586b7cad9a4b63f1af7ee7c9d7cfc8
Yash Patel <mailforyash04@gmail.com>
2022-05-28 23:54:35 +0530

    📱 Some basic design and added sample json data to feed news

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,25 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is news component
-                <NewsItem />
+            <div className='container my-3'>
+                <h2>NewsStar top headlines</h2>
+                <div className="row">
+                    <div className="col-md-4 mt-2">
+                        <NewsItem title="My title" description="My description"/>
+                    </div>
+                    <div className="col-md-4 mt-2">
+                        <NewsItem title="My title" description="My description"/>
+                    </div>
+                    <div className="col-md-4 mt-2">
+                        <NewsItem title="My title" description="My description"/>
+                    </div>
+                    <div className="col-md-4 mt-2">
+                        <NewsItem title="My title" description="My description"/>
+                    </div>
+                    <div className="col-md-4 mt-2">
+                        <NewsItem title="My title" description="My description"/>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
+        let {title, description} = this.props
         return (
             <div>
-                This the news item component
+                <div className="card" style={{width: '18rem'}}>
+                    <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}</p>
+                            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                </div>
             </div>
         )
     }
