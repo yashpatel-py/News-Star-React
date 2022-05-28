03893b40fa1cd5a61a9f79136dade11fb7bf7cfb
Yash Patel <mailforyash04@gmail.com>
2022-05-28 23:23:00 +0530

    Initial Setup
    This is just an initial react app push

---
+++ src/components/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+    render() {
+        return (
+            <div>
+                This is news component
+                <NewsItem />
+            </div>
+        )
+    }
+}
+
+export default News
---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        return (
+            <div>
+                This the news item component
+            </div>
+        )
+    }
+}
+
+export default NewsItem
