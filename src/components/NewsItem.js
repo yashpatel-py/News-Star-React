4e825115df6e3b124198f1aa96d0515a8a2afa39
yash-2115 <mailforyash04@gmail.com>
2022-05-29 06:27:16 -0700

    ✅ some small chnages

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
     render() {
         let {title, description, imageUrl} = this.props
         return (
@@ -10,7 +11,7 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                        <a href="/newsdetail/" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
